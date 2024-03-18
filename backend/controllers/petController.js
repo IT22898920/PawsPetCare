b44862a7ad8e8685a95165a3c6623d5710fa819b
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2024-03-18 11:57:45 +0530

    create backend for the update, delete
    create backend functions for the update delete

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -57,6 +57,110 @@ const getPets = asyncHandler(async(req,res)=> {
     res.status(300).json(petAdoption)
 });
 
+
+const getPet = asyncHandler(async (req, res) => {
+  const pet = await PetAdoption.findById(req.params.id);
+  // If the pet doesn't exist
+  if (!pet) {
+    res.status(404);
+    throw new Error("Pet not found");
+  }
+  // Check if the user is authorized to access the pet
+  if (pet.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  res.status(200).json(pet);
+});
+
+//delete pets
+const deletePet = asyncHandler(async (req,res) =>{
+
+  const pet = await PetAdoption.findById(req.params.id);
+  // If the pet doesn't exist
+  if (!pet) {
+    res.status(404);
+    throw new Error("Pet not found");
+  }
+  // Check if the user is authorized to access the pet
+  if (pet.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  await pet.remove();
+  res.status(200).json({message : "Pet deleted"});
+});
+
+//update pet
+
+const updatePet = asyncHandler(async (req, res) => {
+  const { name, category, price, description } = req.body;
+  const { id } = req.params;
+
+  // Find the pet by ID
+  const pet = await PetAdoption.findById(id);
+
+  // If the pet doesn't exist
+  if (!pet) {
+    res.status(404);
+    throw new Error("Pet not found");
+  }
+
+  // Check if the user is authorized to update the pet
+  if (pet.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "pinvent App",
+        resource_type: "image"
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2) // 2 is decimal
+    };
+  }
+
+  // Update pet
+  const updatedPet = await PetAdoption.findByIdAndUpdate(
+    id,
+    {
+      user: req.user.id,
+      name,
+      category,
+      price,
+      description,
+      image: Object.keys(fileData).length === 0 ?  fileData || pet?.image :
+      fileData,// Preserve existing image if new image is not uploaded
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.status(200).json(updatedPet);
+});
+
 module.exports = {
-  createPet,getPets,
+  createPet,
+  getPets,
+  getPet,
+  deletePet,
+  updatePet
 };
+
