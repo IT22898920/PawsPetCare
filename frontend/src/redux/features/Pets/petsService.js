740150b723bf48fea349b298c31068a1a55e1610
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2024-03-18 21:57:25 +0530

    creating petslice.js and petservices.js
    creating a petSlice and petservices.js

---
+++ frontend/src/redux/features/Pets/petsService.js
@@ -0,0 +1,23 @@
+
+import axios from "axios";
+
+ export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+ const API_URl = `${BACKEND_URL}/api/pets`
+
+ //Create new Pets
+  const createPets = async (formData) =>{
+    const response = await axios.post(API_URl,formData)
+    return response.data
+ 
+ };
+
+ const petsService = {
+
+    createPets,
+ };
+
+ export default petsService;
+
+
+
