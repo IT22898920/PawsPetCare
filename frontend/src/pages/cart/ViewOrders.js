83e23ba9dbc346678cab6538f8027f040e5e1d78
NethmiNim <nethmithambawita2001@gmail.com>
2024-04-11 08:17:11 +0530

    create vieworders.css

--- frontend/src/pages/cart/ViewOrders.js
+++ frontend/src/pages/cart/ViewOrders.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { selectEmail, selectName } from "../../redux/features/auth/authSlice";
-
+import "./ViewOrders.css";
 import { getUser } from "../../services/authService";
 
 const ViewOrders = () => {
