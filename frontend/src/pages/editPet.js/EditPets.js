ba0c5afced21b608ee74d3440363b8962cd33e69
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2024-04-25 22:53:48 +0530

    create frontend items

---
+++ frontend/src/pages/editPet.js/EditPets.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
+import petForm from "../../components/PetForm/PetForm"
+
+const EditPets = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditPets
