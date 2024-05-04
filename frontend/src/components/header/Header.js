29e6dc363ff79c3330835c8e85014afe2aaf085b
Ravindu Pasanjith <IT22898920@my.sliit.lk>
2024-05-05 01:15:46 +0530

    edit the clint side page

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
 import { logoutUser } from "../../services/authService";
+import "./Header.css"
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const name = useSelector(selectName);
+  const [showLogin, setShowLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleClintPageAccess = () => {
+    if (email === "lalith@gmail.com" && password === "22222") {
+      navigate('/cout', { state: { allowed: true } }); 
+    } else {
+      alert("Invalid credentials!");
+    }
+    setShowLogin(false); 
+  };
+  
   const logout = async () => {
     await logoutUser();
     await dispatch(SET_LOGIN(false));
@@ -22,26 +35,37 @@ const Header = () => {
           <span className="--fw-thin">Welcome, </span>
           <span className="--color-danger">{name}</span>
         </h3>
-        <button 
-  onClick={() => navigate('/admin/AllProductList')} 
-  style={{
-    backgroundColor: '#b624ff', // Updated background color
-    color: 'white', // Text color
-    padding: '10px 20px', // Padding: 10px top and bottom, 20px left and right
-    border: 'none', // No border
-    borderRadius: '5px', // Rounded corners
-    cursor: 'pointer', // Pointer cursor on hover
-    // Add any additional styles here
-  }}
-  className="--btn --btn-AllProductList">
-  AllProductList
-</button>
+        <button onClick={() => navigate('/admin/AllProductList')} className="--btn --btn-AllProductList">
+          All Product List
+        </button>
 
+        <button onClick={() => setShowLogin(true)} className="--btn --btn-AllProductList">
+          Clint Page
+        </button>
 
         <button onClick={logout} className="--btn --btn-danger">
           Logout
         </button>
       </div>
+      {showLogin && (
+        <div>
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClintPageAccess}>
+            Login
+          </button>
+        </div>
+      )}
       <hr />
     </div>
   );
