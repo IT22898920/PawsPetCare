7f20d48caa0735dafd014cf1b183a021e751a13b
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2024-05-07 11:51:18 +0530

    create report genaration

--- frontend/src/redux/features/Pets/petFilterSlice.js
+++ frontend/src/redux/features/Pets/petFilterSlice.js
@@ -1,24 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    filteredPets: []
+  filteredPets: []
 };
 
 const petFilterSlice = createSlice({
-    name: "petFilter",
-    initialState,
-    reducers: {
-      FILTER_PETS(state, action) {
-        const { pets, SearchPet } = action.payload;
-        const tempPets = pets.filter((pet) => {
-            // Check if pet and pet.name are defined before accessing them
-            return pet && pet.name && pet.name.toLowerCase().includes(SearchPet.toLowerCase());
-        });
-    
-        state.filteredPets = tempPets;
+  name: "petFilter",
+  initialState,
+  reducers: {
+    FILTER_PETS(state, action) {
+      const { pets, searchPet } = action.payload;
+
+      // Check if searchPet is defined before calling toLowerCase
+      const searchTerm = searchPet ? searchPet.toLowerCase() : '';
+
+      const tempPets = pets.filter((pet) => {
+        // Check if pet and pet.name are defined before accessing them
+        return pet && pet.name && pet.name.toLowerCase().includes(searchTerm);
+      });
+
+      state.filteredPets = tempPets;
     }
-    
-    }
+  }
 });
 
 export const { FILTER_PETS } = petFilterSlice.actions;
